71d46d4fa27efc4c8e255e1a13afe9201163488d
supElement <cglearned@163.com>
2025-12-05 00:56:04 +0800

    Update version and add MaskStrokeElement

--- __init__.py
+++ __init__.py
@@ -1,22 +1,25 @@
 # __init__.py
-__version__ = "0.0.6" 
+__version__ = "0.0.7" 
 
 from .random_chars import RandomCharacterGenerator
 from .empty_image_rgb import EmptyImageRGB
 from .text_line_break import TextLineBreak
+from .mask_stroke import MaskStrokeElement
 
 WEB_DIRECTORY = "./js"
 
 NODE_CLASS_MAPPINGS = {
     "RandomCharacterGenerator": RandomCharacterGenerator,
     "EmptyImageRGB_Element": EmptyImageRGB,
-    "TextLineBreak_Element": TextLineBreak
+    "TextLineBreak_Element": TextLineBreak,
+    "MaskStroke_Element": MaskStrokeElement
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "RandomCharacterGenerator": "Random Chars (Append)",
     "EmptyImageRGB_Element": "Empty Image RGB",
-    "TextLineBreak_Element": "Text Line Break"
+    "TextLineBreak_Element": "Text Line Break",
+    "MaskStroke_Element": "Mask Stroke"
 }
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]

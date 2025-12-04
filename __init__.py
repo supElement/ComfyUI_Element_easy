3a581754f055a0b7849cf29eba3aac487b85ed43
supElement <cglearned@163.com>
2025-12-04 17:25:19 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,10 @@
+# __init__.py
+__version__ = "1.2.3" 
+
 from .random_chars import RandomCharacterGenerator
 from .empty_image_rgb import EmptyImageRGB
 from .text_line_break import TextLineBreak
 
-# ==============================================================================
-# 关键修改：告诉 ComfyUI 前端文件在哪里
-# "." 表示当前目录，"./js" 表示当前目录下的 js 文件夹
-# ==============================================================================
 WEB_DIRECTORY = "./js"
 
 NODE_CLASS_MAPPINGS = {
@@ -20,5 +19,4 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "TextLineBreak_Element": "Text Line Break"
 }
 
-# 记得把 WEB_DIRECTORY 加到这里
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]

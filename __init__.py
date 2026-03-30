608a2b1f0b9ba0be4249d307a4da25d3ed5b81cc
@supElement <cglearned@163.com>
2026-03-30 10:33:14 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 # __init__.py
-__version__ = "1.3.2" 
+__version__ = "1.3.8" 
 
 from .random_chars import RandomCharacterGenerator
 from .empty_image_rgb import EmptyImageRGB
@@ -9,7 +9,6 @@ from .mask_noise_element import MaskNoiseInjectionElement
 from .image_pad_blur import ImagePadBlur
 from .chessboard import ChessboardPattern_Element
 from .black_white_color import BlackWhiteColor
-from .Element_SigmaGraph import Element_SigmaGraph
 from .frame_calc import FrameCalculator
 from .Image_Size import ElementEasyImageSize
 from .Element_SigmaGraph_Curve import Element_SigmaGraph_Curve
@@ -30,7 +29,6 @@ NODE_CLASS_MAPPINGS = {
     "ImagePadBlur_Element": ImagePadBlur,
     "ChessboardPattern_Element": ChessboardPattern_Element,
     "BlackWhiteColor_Element": BlackWhiteColor,
-    "Element_SigmaGraph": Element_SigmaGraph,
     "FrameCalculator_Element": FrameCalculator,
     "ElementEasyImageSize": ElementEasyImageSize,
     "Element_SigmaGraph_Curve": Element_SigmaGraph_Curve,
@@ -51,10 +49,9 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "ImagePadBlur_Element": "Image Pad & Blur",
     "ChessboardPattern_Element": "chessboard",
     "BlackWhiteColor_Element": "BlackWhiteColor",
-    "Element_SigmaGraph": "Element_SigmaGraph",
     "FrameCalculator_Element": "Frame Calculator",
     "ElementEasyImageSize": "ImageSize Div",
-    "Element_SigmaGraph_Curve": "Element_SigmaGraph (Curve)",
+    "Element_SigmaGraph_Curve": "Element_SigmaGraph",
     "Element_ImageCurve": "Element ImageCurve",
     "ElementHueSat": "Element HueSat",
     "ElementHueHue": "Element HueHue",
